0da6fa509bb098160bbc33463e103ccd3ac8deec
steven2015 <steven2015@steven>
2017-08-12 09:34:23 -0500

    commit inicial

---
+++ src/ejerciciouno/EjercicioUno.java
@@ -0,0 +1,18 @@
+
+package ejerciciouno;
+
+/**
+ *
+ * @author steven2015
+ */
+public class EjercicioUno {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("HOLA MUNDO DESDE LA UDEC");
+    }
+    
+}
